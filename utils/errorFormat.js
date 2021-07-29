bfb77a6db51e776686d0fe504f9c8bcf7d856d33
Mostafa Hisham <mmham3919@gmail.com>
2021-07-29 18:41:30 +0200

    adding error Format utils

---
+++ utils/errorFormat.js
@@ -0,0 +1,10 @@
+const errorFormat = (error, addStack = false) => ({
+	name: error.name,
+	message: error.message,
+	...(addStack ? {
+		stack: error?.stack?.split('\n').map((ele) => ele.trim()),
+	} : {})
+});
+
+
+module.export =  errorFormat;
