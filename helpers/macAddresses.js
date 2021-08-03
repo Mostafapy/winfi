47463449ce07144985fcfe1683a9e4452469eeb0
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-03 14:13:25 +0200

    add macAddresses helper function

---
+++ helpers/macAddresses.js
@@ -0,0 +1,4 @@
+const generateMacAddress = () =>  'XX:XX:XX:XX:XX:XX'.replace(/X/g, () => { return '0123456789ABCDEF'.charAt(Math.floor(Math.random() * 16)); });
+
+module.exports = { generateMacAddress };
+
