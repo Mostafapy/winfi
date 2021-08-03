054d24100b6fcb09ca64f75fc1b46f8bffecc45d
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-03 17:34:32 +0200

    eslint configure for database

--- config/db.js
+++ config/db.js
@@ -1,30 +1,33 @@
 const mysql = require('mysql2');
 
-
 // remove " char from password e.g. "pass" => pass
 // winficocWinfi DB
-const winficocWinfiDBPassword = String(process.env.WINFICOC_WINFI_DB_MYSQL_PASSWORD).replace('"', '');
+const winficocWinfiDBPassword = String(
+  process.env.WINFICOC_WINFI_DB_MYSQL_PASSWORD,
+).replace('"', '');
 
 // radius DB
-const radiusDBPassword = String(process.env.Radius_DB_MYSQL_PASSWORD).replace('"', '');
+const radiusDBPassword = String(process.env.Radius_DB_MYSQL_PASSWORD).replace(
+  '"',
+  '',
+);
 
 // Creating connection pool of winficocWinfi DB
 const winficocWinfiPool = mysql.createPool({
-	host: process.env.WINFICOC_WINFI_DB_MYSQL_HOST,
-	user: process.env.WINFICOC_WINFI_DB_MYSQL_USER,
-	password: winficocWinfiDBPassword,
-	database: process.env.WINFICOC_WINFI_DB_MYSQL_DBNAME,
-	waitForConnections: true,
+  host: process.env.WINFICOC_WINFI_DB_MYSQL_HOST,
+  user: process.env.WINFICOC_WINFI_DB_MYSQL_USER,
+  password: winficocWinfiDBPassword,
+  database: process.env.WINFICOC_WINFI_DB_MYSQL_DBNAME,
+  waitForConnections: true,
 });
-
 
 // Creating connection pool of radius DB
 const radiusPool = mysql.createPool({
-	host: process.env.RADIUS_DB_MYSQL_HOST,
-	user: process.env.RADIUS_DB_MYSQL_USER,
-	password: radiusDBPassword,
-	database: process.env.RADIUS_DB_MYSQL_DBNAME,
-	waitForConnections: true,
+  host: process.env.RADIUS_DB_MYSQL_HOST,
+  user: process.env.RADIUS_DB_MYSQL_USER,
+  password: radiusDBPassword,
+  database: process.env.RADIUS_DB_MYSQL_DBNAME,
+  waitForConnections: true,
 });
 
 // Create promise from pool connection
@@ -33,7 +36,6 @@ const winficocWinfiDBPromisePool = winficocWinfiPool.promise();
 const RadiusDBPromisePool = radiusPool.promise();
 
 module.exports = {
-	winficocWinfiDBPromisePool,
-	RadiusDBPromisePool
+  winficocWinfiDBPromisePool,
+  RadiusDBPromisePool,
 };
-
