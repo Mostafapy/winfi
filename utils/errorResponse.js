8b17c5380830b15939d01654d957fa24500cbc73
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-04 12:57:04 +0200

    adding ErrorResponse class util

---
+++ utils/errorResponse.js
@@ -0,0 +1,26 @@
+/**
+ * @class
+ * @classdesc Error response class to be used over the app.
+ */
+class ErrorResponse extends Error {
+  /**
+   * Create an error response.
+   * @param {string} message - The error message.
+   * @param {number} status - The error code.
+   * @param {string} stack - The error stack.
+   */
+  constructor(message, stack = 'Not Specified') {
+    super(message);
+
+    if (message.startsWith('wrong')) {
+      this.status = 422;
+    } else if (message.includes('exists')) {
+      this.status = message.includes('not') ? 404 : 409;
+    } else {
+      this.status = 400;
+    }
+    this.stack = stack;
+  }
+}
+
+module.exports = { ErrorResponse };
