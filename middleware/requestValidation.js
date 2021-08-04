ee13791ebeb10106d8752cb7960fe908cb05ab76
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-04 11:05:36 +0200

    add request validation middleware

---
+++ middleware/requestValidation.js
@@ -0,0 +1,21 @@
+module.exports = (schema) => {
+  return (req, res, next) => {
+    const validations = [];
+    [('headers', 'params', 'query', 'body')].forEach((key) => {
+      if (schema[key]) {
+        const validation = schema[key].validate(req[key]);
+        if (validation.error) {
+          validations.push(validation.error.message);
+        }
+      }
+    });
+    if (validations.length) {
+      res.status(422).json({
+        status: false,
+        err: validations.join(),
+        data: null,
+      });
+    }
+    next();
+  };
+};
