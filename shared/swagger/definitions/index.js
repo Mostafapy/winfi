d2da7ab42bb88c76b4ca88297e232f23d892dc7c
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-04 19:49:27 +0200

    adding index files in swager

---
+++ shared/swagger/definitions/index.js
@@ -0,0 +1,7 @@
+const userDefinition = require('./user');
+
+const definitions = {
+  userDefinition,
+};
+
+module.exports = { definitions };
---
+++ shared/swagger/docs/index.js
@@ -0,0 +1,5 @@
+const userDocs = require('./user');
+const docs = {
+  userDocs,
+};
+module.exports = { docs };
