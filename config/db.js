fd55327837a92d9e49482d16d698d15ff2d45bed
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-15 11:57:06 +0200

    adding ssh config connections

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const { Client } = require('ssh2');
 
 // remove " char from password e.g. "pass" => pass
 // winficocWinfi DB
@@ -28,11 +29,69 @@ const radiusPool = mysql.createPool({
   database: process.env.RADIUS_DB_MYSQL_DBNAME,
 });
 
-// Create promise from pool connection
+// Create promise from pool WINFI DB connection
 const winficocWinfiDBPromisePool = winficocWinfiPool.promise();
 
-const radiusDBPromisePool = radiusPool.promise();
+// SSH Connection for production
 
+// create an instance of SSH Client
+const sshClient = new Client();
+
+const dbServer = {
+  host: process.env.RADIUS_DB_MYSQL_HOST,
+  port: 3306,
+  user: process.env.RADIUS_DB_MYSQL_USER,
+  password: process.env.RADIUS_DB_MYSQL_PASSWORD,
+  database: process.env.RADIUS_DB_MYSQL_DBNAME,
+};
+const tunnelConfig = {
+  host: process.env.RADIUS_SSH_HOST,
+  port: 22,
+  username: process.env.RADIUS_SSH_USER,
+  password: Buffer.from(process.env.RADIUS_SSH_PRIVATE_KEY, 'utf-8'),
+};
+const forwardConfig = {
+  srcHost: '127.0.0.1',
+  srcPort: 3306,
+  dstHost: dbServer.host,
+  dstPort: dbServer.port,
+};
+
+const radiusDBSSHConnection = new Promise((resolve, reject) => {
+  sshClient
+    .on('ready', () => {
+      sshClient.forwardOut(
+        forwardConfig.srcHost,
+        forwardConfig.srcPort,
+        forwardConfig.dstHost,
+        forwardConfig.dstPort,
+        (err, stream) => {
+          if (err) reject(err);
+          const updatedDbServer = {
+            ...dbServer,
+            stream,
+          };
+          const connection = mysql.createConnection(updatedDbServer);
+          connection.connect((error) => {
+            if (error) {
+              reject(error);
+            }
+            resolve(connection);
+          });
+        },
+      );
+    })
+    .connect(tunnelConfig);
+});
+
+// Create promise from pool RADIUS DB connection
+let radiusDBPromisePool;
+
+if (process.env.NODE_ENV == 'production') {
+  radiusDBPromisePool = radiusDBSSHConnection;
+} else {
+  radiusDBPromisePool = radiusPool.promise();
+}
 module.exports = {
   winficocWinfiDBPromisePool,
   radiusDBPromisePool,
