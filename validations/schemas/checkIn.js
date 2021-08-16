1de129254645aa812b40555649d4c0d0105d4069
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-17 01:55:52 +0200

    change restrictions field to package in checkInSchema

--- validations/schemas/checkIn.js
+++ validations/schemas/checkIn.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const checkInSchema = Joi.object().required().keys({
   mobile: Joi.string().required(),
   location: Joi.string().required(),
-  restrictions: Joi.any(),
+  package: Joi.string().required(),
 });
 
 module.exports = { checkInSchema };
