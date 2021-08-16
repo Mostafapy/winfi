40927b532981e595884a95ee8c1446971e0c9282
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-16 22:59:09 +0200

    add passwords helper functions

--- helpers/passwords.js
+++ helpers/passwords.js
@@ -8,4 +8,36 @@ const crypto = require('crypto');
 const md5HashPassword = (password) =>
   crypto.createHash('md5').update(password).digest('hex');
 
-module.exports = { md5HashPassword };
+/**
+ * Helper function to random password using MD5
+ * @param {number} passwordLength
+ * @returns { String } generated password
+ */
+const generateNewPassword = (passwordLength) => {
+  var numberChars = '0123456789';
+  var upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+  var allChars = numberChars + upperChars + lowerChars;
+  var randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numberChars;
+  randPasswordArray[1] = upperChars;
+  randPasswordArray[2] = lowerChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 3);
+  return shuffleArray(
+    randPasswordArray.map(function (x) {
+      return x[Math.floor(Math.random() * x.length)];
+    }),
+  ).join('');
+};
+
+const shuffleArray = (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+module.exports = { md5HashPassword, generateNewPassword };
