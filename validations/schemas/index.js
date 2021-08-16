3e12ce63002a46aebc788405b2e1f40e1c908e05
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-17 01:56:36 +0200

    import identifyAppSchema in index file

--- validations/schemas/index.js
+++ validations/schemas/index.js
@@ -1,9 +1,11 @@
 const { checkInSchema } = require('./checkIn');
 const { createUserSchema } = require('./createUser');
 const { loginSchema } = require('./login');
+const { identifyAppSchema } = require('./identifyApp');
 
 module.exports = {
   createUserSchema,
   checkInSchema,
   loginSchema,
+  identifyAppSchema,
 };
