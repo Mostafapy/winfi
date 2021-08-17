288fa2d6836deeab7bd87b0c59e7549a70d5876a
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-17 02:11:36 +0200

    fix fields for checkIn

--- validations/schemas/checkIn.js
+++ validations/schemas/checkIn.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const checkInSchema = Joi.object().required().keys({
   mobile: Joi.string().required(),
   location: Joi.string().required(),
-  package: Joi.string().required(),
+  packageName: Joi.string().required(),
+  packageValue: Joi.string().required(),
 });
 
 module.exports = { checkInSchema };
