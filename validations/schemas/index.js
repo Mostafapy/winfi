5975ab5d0be8c28e41d06d8d52f4edf2e4e84dc3
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-17 02:32:23 +0200

    adding topUpSchemato index file

--- validations/schemas/index.js
+++ validations/schemas/index.js
@@ -2,10 +2,12 @@ const { checkInSchema } = require('./checkIn');
 const { createUserSchema } = require('./createUser');
 const { loginSchema } = require('./login');
 const { identifyAppSchema } = require('./identifyApp');
+const { topUpSchema } = require('./topUp');
 
 module.exports = {
   createUserSchema,
   checkInSchema,
   loginSchema,
   identifyAppSchema,
+  topUpSchema,
 };
