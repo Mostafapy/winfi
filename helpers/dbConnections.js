8e5775837604c96f9dd0f6d2b98c8afaafd1304b
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-17 15:23:21 +0200

    rename connectionPoolPromise to dbConnection

---
+++ helpers/dbConnections.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql2');
+
+const connectionPoolPromise = async (connectionPool, logger, moduleName) => {
+  try {
+    if (process.env.NODE_ENV == 'production') {
+      const pool = await connectionPool;
+      return Promise.resolve(pool);
+    }
+
+    return Promise.resolve(connectionPool);
+  } catch (err) {
+    logger.error(err.message, err);
+    err.message = `${moduleName},${err.message}`;
+    return Promise.reject(err);
+  }
+};
+
+const sshDBRemoteConnection = (sshConf, sqlConf, sshClient) => {
+  return new Promise((resolve, reject) => {
+    sshClient
+      .on('ready', function () {
+        sshClient.forwardOut(
+          '127.0.0.1',
+          24000,
+          '127.0.0.1',
+          3306,
+          function (err, stream) {
+            if (err) reject(err);
+
+            sqlConf.stream = stream;
+            resolve(mysql.createPool(sqlConf).promise());
+          },
+        );
+      })
+      .connect(sshConf);
+  });
+};
+
+module.exports = { connectionPoolPromise, sshDBRemoteConnection };
