ef32fa476394d38d00ec01dce20a148a0f57403d
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-19 21:44:30 +0200

    fix error handling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,18 +2,6 @@
 const { Logger } = require('../utils/logger');
 
 const errorHandler = (err, req, res, next) => {
-  const errorMessageDetails = err.message.split(',');
-
-  const moduleName = errorMessageDetails[0];
-
-  const message = errorMessageDetails[1];
-
-  const logger = new Logger(moduleName);
-
-  err.message = message;
-
-  logger.error(err.message, err);
-
   return res.status(err.status || 500).json({
     success: false,
     message: err.message || 'Server Error',
