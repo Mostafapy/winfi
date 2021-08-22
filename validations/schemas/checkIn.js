534a4617807b06964ec564edbfe8d2b2a6288737
mostafa hisham <mostafa.hisham@forus.cc>
2021-08-22 09:25:09 +0200

    fix validation schema of checkin and topup

--- validations/schemas/checkIn.js
+++ validations/schemas/checkIn.js
@@ -3,8 +3,7 @@ const Joi = require('joi');
 const checkInSchema = Joi.object().required().keys({
   mobile: Joi.string().required(),
   location: Joi.string().required(),
-  packageName: Joi.string().required(),
-  packageValue: Joi.string().required(),
+  groupName: Joi.string().required(),
 });
 
 module.exports = { checkInSchema };
--- validations/schemas/topUp.js
+++ validations/schemas/topUp.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const topUpSchema = Joi.object().required().keys({
   mobile: Joi.string().required(),
   location: Joi.string().required(),
-  packageValue: Joi.string().required(),
+  groupName: Joi.string().required(),
+  topUpValue: Joi.number().required(),
 });
 
 module.exports = { topUpSchema };
