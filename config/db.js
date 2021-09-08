c7cbf61090b5701d57875410c3ad5f7d2bf36630
mostafa hisham <mostafa.hisham@forus.cc>
2021-09-08 12:00:47 +0200

    fix db config for radius connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,4 @@
 const mysql = require('mysql2');
-const fs = require('fs');
-const path = require('path');
-const sshClient = require('ssh2').Client;
 
 // remove " char from password e.g. "pass" => pass
 // winficocWinfi DB
@@ -28,58 +25,12 @@ const winficocWinfiPool = mysql.createPool({
 const winficocWinfiDBPromisePool = winficocWinfiPool.promise();
 
 // Creating connection pool of radius DB
-const radiusPool = mysql.createPool({
+const radiusDBPromisePool = mysql.createPool({
   host: process.env.RADIUS_DB_MYSQL_HOST,
   user: process.env.RADIUS_DB_MYSQL_USER,
   password: radiusDBPassword,
   database: process.env.RADIUS_DB_MYSQL_DBNAME,
 });
-
-let radiusDBPromisePool;
-
-if (process.env.NODE_ENV == 'production') {
-  // ssh connection
-  const sshConf = {
-    host: process.env.RADIUS_SSH_HOST,
-    port: 22,
-    username: process.env.RADIUS_SSH_USER,
-    privateKey: fs.readFileSync(
-      path.resolve(__dirname, process.env.RADIUS_SSH_PRIVATE_KEY_PATH),
-    ),
-  };
-
-  const sqlConf = {
-    port: 3306,
-    user: process.env.RADIUS_DB_MYSQL_USER,
-    password: radiusDBPassword,
-    database: process.env.RADIUS_DB_MYSQL_DBNAME,
-  };
-
-  const ssh = new sshClient();
-  const radiusDBSSHConnection = new Promise((resolve, reject) => {
-    ssh
-      .on('ready', function () {
-        ssh.forwardOut(
-          '127.0.0.1',
-          24000,
-          '127.0.0.1',
-          3306,
-          function (err, stream) {
-            if (err) reject(err);
-
-            sqlConf.stream = stream;
-            resolve(mysql.createPool(sqlConf).promise());
-          },
-        );
-      })
-      .connect(sshConf);
-  });
-
-  // Create promise from pool RADIUS DB connection
-  radiusDBPromisePool = radiusDBSSHConnection;
-} else {
-  radiusDBPromisePool = radiusPool;
-}
 
 module.exports = {
   winficocWinfiDBPromisePool,
