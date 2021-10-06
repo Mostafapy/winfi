85dd7958575e7ab864c959c2e69a418a78749a2a
mostafa hisham <mostafa.hisham@forus.cc>
2021-10-06 23:01:22 +0200

    fix generateCodesAndOtps helper function

--- helpers/codesAndOtps.js
+++ helpers/codesAndOtps.js
@@ -1,8 +1,10 @@
 /**
  * Helper function to generate verification codes
- * @returns { String } 5 digits code or OTPS
+ * @returns { String } 6 digits code or OTPS
  */
 const generateCodesAndOtps = () =>
-  Math.floor(Math.random() * (9 * Math.pow(10, 5))) + Math.pow(10, 5);
+  Math.floor(
+    Math.pow(10, 5) + Math.random() * (Math.pow(10, 6) - Math.pow(10, 5) - 1),
+  );
 
 module.exports = { generateCodesAndOtps };
