da917e1d1884831289d2ac60e255cdfbeb77ef7e
mostafa hisham <mostafa.hisham@forus.cc>
2021-10-06 22:28:56 +0200

    remove identifyApp schema validation

--- validations/schemas/identifyApp.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const identifyAppSchema = Joi.object().required().keys({
-  mobile: Joi.string().required(),
-  uuid: Joi.string().required(),
-});
-
-module.exports = { identifyAppSchema };
