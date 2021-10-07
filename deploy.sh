7a5cfaa2aa869db32847a0c20b8756846b8e2144
mostafa hisham <mostafa.hisham@forus.cc>
2021-10-07 13:17:03 +0200

    fix bash script

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ echo "--------------------------------------------------------------------"
 echo "------------------------------- WINFI -------------------------------"
 echo "--------------------------------------------------------------------"
 
-echo "Pull New Changes (main)"
+echo "Pull New Changes (main or dev)"
 sleep 1
-git pull origin main
+git pull origin $1
 
-echo "Stop Docker Images (Production)"
+echo "Stop Docker Images (Production or Development)"
 docker-compose -p winfi-api stop
 
-echo "Build And Deploy New Docker Images (Production)"
+echo "Build And Deploy New Docker Images (Production or Development)"
 sleep 1
 docker-compose -p winfi-api up --build -d
 
