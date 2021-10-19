e30f28cb73d3232a5db98ec8272a457c5ae07299
mostafa hisham <mostafa.hisham@forus.cc>
2021-10-19 13:06:05 +0200

    add browser and browser version in the login schema

--- validations/schemas/login.js
+++ validations/schemas/login.js
@@ -2,6 +2,8 @@ const Joi = require('joi');
 
 const loginSchema = Joi.object().required().keys({
   otp: Joi.string().required(),
+  browser: Joi.string().required(),
+  browserVersion: Joi.string().required(),
 });
 
 module.exports = { loginSchema };
